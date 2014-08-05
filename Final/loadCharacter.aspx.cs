073d2167c3bc434019e745f2741d201d0a005f09
djspx <parracho@sheridancollege.ca>
2014-08-05 19:01:58 -0400

    Load Character Updates
    Load Character as an Object through DataView

--- Final/loadCharacter.aspx.cs
+++ Final/loadCharacter.aspx.cs
@@ -6,11 +6,17 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Drawing;
+using System.Data;
 
 namespace Final
 {
     public partial class WebForm4 : System.Web.UI.Page
     {
+
+        DataView dv;
+        DataTable dt;
+        DataSet ds;
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -21,7 +27,10 @@ namespace Final
         {
             try // need try-catch to catch SqlExceptions
             {
-                characterSource.Select(DataSourceSelectArguments.Empty);
+                // this fires the characterSource_Selected event below
+                // store what is returned by SELECT into a DataView. More in the characterSource_Selected event
+                dv = (DataView)characterSource.Select(DataSourceSelectArguments.Empty);
+
             }
             catch (SqlException ex)
             {
@@ -39,11 +48,29 @@ namespace Final
                 LabelMsg.ForeColor = Color.Green;
                 LabelMsg.Text = "Character successfully loaded.";
                 // create character object with attributes from db
-            }
-            else
-            {
-                LabelMsg.ForeColor = Color.Red;
-                LabelMsg.Text = "Error: This character could not be loaded.";
+                dt = dv.ToTable();
+                ds = new DataSet();
+                ds.Tables.Add(dt);
+
+                DataRow dRow = ds.Tables[0].Rows[1];
+                string characterName = dRow.ItemArray.GetValue(1).ToString();
+                string characterClass = dRow.ItemArray.GetValue(2).ToString();
+                int attack = (int)dRow.ItemArray.GetValue(3);
+                int health = (int)dRow.ItemArray.GetValue(4);
+                int damage = (int)dRow.ItemArray.GetValue(5);
+                int days = (int)dRow.ItemArray.GetValue(6);
+                int enemyLevel = (int)dRow.ItemArray.GetValue(7);
+
+                Character playerChar = new Character();
+                playerChar.Name = characterName;
+                playerChar.Class = characterClass;
+                playerChar.Attack = attack;
+                playerChar.Health = health;
+                playerChar.DamageTaken = damage;
+                //playerChar.Days = days;
+                //playerChar.EnemyLevel = health;
+
+                Session.Add("Character", playerChar);
             }
         }
     }
